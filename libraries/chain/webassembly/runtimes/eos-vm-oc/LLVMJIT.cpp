04c9c820529564944e129737d9588d5fa61a624f
rwcii <rwcii@users.noreply.github.com>
2026-03-15 05:39:09 +0000

    AArch64: Enable DUMP_UNOPTIMIZED_MODULE for f64 debug

--- libraries/chain/webassembly/runtimes/eos-vm-oc/LLVMJIT.cpp
+++ libraries/chain/webassembly/runtimes/eos-vm-oc/LLVMJIT.cpp
@@ -73,7 +73,7 @@ namespace llvm { namespace orc {
 }}
 #endif
 
-#define DUMP_UNOPTIMIZED_MODULE 0
+#define DUMP_UNOPTIMIZED_MODULE 1
 #define VERIFY_MODULE 1
 #define DUMP_OPTIMIZED_MODULE 1
 #define PRINT_DISASSEMBLY 0
